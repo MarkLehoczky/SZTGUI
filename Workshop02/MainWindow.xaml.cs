1b7c8bd3e5886b44c51e0754c2baa20a95b99d43
Mark <marklehoczky@gmail.com>
2024-03-05 22:08:58 +0100

    Workshop02 completed

---
+++ Workshop02/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Workshop02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Question> questionList;
+
+        public MainWindow()
+        {
+            questionList = JsonConvert.DeserializeObject<List<Question>>(File.ReadAllText("questions.json")) ?? new List<Question>();
+            InitializeComponent();
+        }
+
+        private void wrap_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < questionList.Count; i++)
+            {
+                var image = new Image
+                {
+                    Width = ActualWidth / 7,
+                    Source = new BitmapImage(new Uri("image.jpg", UriKind.RelativeOrAbsolute))
+                    
+                };
+                Button button = new Button()
+                {
+                    Background = Brushes.Transparent,
+                    Content = image,
+                    BorderBrush = Brushes.Black,
+                    BorderThickness = new Thickness(4),
+                    Margin = new Thickness(10),
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    Tag = i
+                };
+                button.Click += OpenQuestion;
+
+                wrap.Children.Add(button);
+            }
+        }
+
+        private void OpenQuestion(object sender, EventArgs e)
+        {
+            if (sender is Button button)
+            {
+                if (new Quiz(questionList[(int)button.Tag]).ShowDialog() == true)
+                    button.BorderBrush = Brushes.Green;
+                else
+                    button.BorderBrush = Brushes.Red;
+
+                button.Click -= OpenQuestion;
+            }
+        }
+    }
+
+    public class Question
+    {
+        public string question { get; set; }
+        public string A { get; set; }
+        public string B { get; set; }
+        public string C { get; set; }
+        public string D { get; set; }
+        public string correct { get; set; }
+    }
+}
---
+++ Workshop02/Quiz.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Workshop02
+{
+    /// <summary>
+    /// Interaction logic for Quiz.xaml
+    /// </summary>
+    public partial class Quiz : Window
+    {
+        Question quizQuestion;
+        DateTime start;
+        public Quiz(Question question)
+        {
+            quizQuestion = question;
+            InitializeComponent();
+
+            label_question.Content = quizQuestion.question;
+            btn_a.Content = quizQuestion.A; btn_a.Tag = "A";
+            btn_b.Content = quizQuestion.B; btn_b.Tag = "B";
+            btn_c.Content = quizQuestion.C; btn_c.Tag = "C";
+            btn_d.Content = quizQuestion.D; btn_d.Tag = "D";
+            start = DateTime.Now;
+            Timer();
+        }
+
+        //Source: https://stackoverflow.com/questions/11719283/how-to-close-auto-hide-wpf-window-after-10-sec-using-timer
+        private void Timer()
+        {
+            var timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1);
+            timer.Tick += TimerResizer;
+            timer.Start();
+        }
+
+        private async void TimerResizer(object sender, EventArgs e)
+        {
+            var size = 700 - 700 * ((DateTime.Now - start).TotalMilliseconds / 60000);
+            if (size < 0)
+            {
+                message = false;
+                Close();
+            }
+
+            time_top.Width = size;
+            time_bottom.Width = size;
+            Timer();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            message = false;
+            if (sender is Button button && (string)button.Tag == quizQuestion.correct)
+                DialogResult = true;
+
+            Close();
+        }
+
+        bool message = true;
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if (message)
+            {
+                var box = MessageBox.Show("Biztos ki akar lépni?", "Kilépés", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (box == MessageBoxResult.No)
+                    e.Cancel = true;
+            }
+        }
+    }
+}
